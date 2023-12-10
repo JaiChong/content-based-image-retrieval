55e3cd0bb8938486a009fc5bb7bb75eaedf23b15
Jaimi Chong <jaimi.chong@gmail.com>
2023-12-10 14:05:03 -0800

    a1,2: `//`ed unused imports
    Tired of seeing them pop up as warnings myb

--- CBIR.java
+++ CBIR.java
@@ -37,11 +37,11 @@
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
+// import java.awt.image.BufferedImage;
 import java.awt.*;
 import java.util.*;
 import java.io.*;
-import java.util.List;
+// import java.util.List;
 
 import javax.swing.*;
 
